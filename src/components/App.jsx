a1296cc9314a1486824d56124e3ad2fe055edb49
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-07-12 14:31:17 -0700

    initial

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MastHeader() {
+  return(
+    <div className='mastHeaderContainer'>
+      hey
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <MastHeader />
+    </div>
+  );
+}
+
+export default App;
