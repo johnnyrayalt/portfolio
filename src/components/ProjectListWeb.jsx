158bb74390a33361076b84fccf5e911eea95966f
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-07-13 18:56:10 -0700

    further nested component views

---
+++ src/components/ProjectListWeb.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ProjectListWeb() {
+  return(
+    <div className='projectListWebContainer'>
+      <p className='text'>Web Component</p>
+      <style jsx>{`
+        .projectListWebContainer {
+          font-family: 'Raleway', sans-serif;
+          font-size:24px;
+          font-weight:bold;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default ProjectListWeb;
