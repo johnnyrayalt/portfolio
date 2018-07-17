70106f36bdd122682e1d71e0b8bc55450dbf9681
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-07-17 09:54:05 -0700

    refactor to remove degisn section and changed fine art to art

---
+++ src/components/ProjectListArt.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ProjectListFineArt() {
+  return(
+    <div className='projectListFineArtContainer'>
+      <p className='text'>Fine Art Component</p>
+      <style jsx>{`
+        .projectListFineArtContainer {
+          font-family: 'Raleway', sans-serif;
+          font-size:24px;
+          font-weight:bold;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default ProjectListFineArt;
