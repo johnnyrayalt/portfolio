289b667faa8de41aaf1c386b754d7894dc4a294e
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-07-17 10:12:19 -0700

    art components

---
+++ src/components/ArtAlphaDecay.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ArtAlphaDecay() {
+  return(
+    <div className='artAlphaDecayContainer'>
+      Alpha Decay
+
+      <style jsx>{`
+        .artAlphaDecayContainer {
+          font-family: 'Raleway', sans-serif;
+          font-size:24px;
+          font-weight:bold;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default ArtAlphaDecay;
---
+++ src/components/ArtOptimizedLivingSpaces.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ArtOptimizedLivingSpaces() {
+  return(
+    <div className='artOptimizedLivingSpacesContainer'>
+      Optimized
+
+      <style jsx>{`
+        .artOptimizedLivingSpacesContainer {
+          font-family: 'Raleway', sans-serif;
+          font-size:24px;
+          font-weight:bold;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default ArtOptimizedLivingSpaces;
