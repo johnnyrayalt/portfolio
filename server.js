3a77a67a2894da4cb5164e94fbbbc4ea8a9ea428
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-07-25 17:23:33 -0700

    prod build config

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/build/'));
 app.listen(process.env.PORT || 8080);
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
