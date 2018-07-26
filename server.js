63f01ed0a3a3e3e13798e53804ffd52ebc45007c
Guest User <Guest@Epicodus-7E.local>
2018-07-26 09:03:54 -0700

    clean up

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 app.use(express.static(__dirname + '/build/'));
 app.listen(process.env.PORT || 8080);
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+    app.use(express.static('/build'));
 }
 app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
